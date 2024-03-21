db536fed4ddb63c07fc5591d8a0b7a40483b859c
root <root@srv9859618635.bitcommand.net>
2024-03-21 18:06:26 +0000

    day two commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{stdout, Write, Stdout};
-
+use std::{time::Duration, io};
 use crossterm::{
     execute,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
@@ -8,8 +8,13 @@ use crossterm::{
     event,
     terminal::size,
     cursor::MoveTo,
+
 };
 
+use crossterm::event::{poll, read, Event};
+use crossterm::event::KeyCode;
+use crossterm::terminal::disable_raw_mode;
+use crossterm::terminal::enable_raw_mode;
 
 struct World{
     player_c : u16,
@@ -27,7 +32,7 @@ fn draw(mut sc: &Stdout , world: &World){
 fn main() -> std::io::Result<()> {
         let mut sc = stdout();
         let (maxc , maxl) = size().unwrap();
-        
+        enable_raw_mode();
 
 
         let mut world = World{
@@ -36,8 +41,31 @@ fn main() -> std::io::Result<()> {
 
         };
         loop{
+
+            if poll(Duration::from_millis(10))? {
+            // It's guaranteed that the `read()` won't block when the `poll()`
+            // function returns `true`
+            let key = read().unwrap();
+            match key {
+                Event::Key(event) => {
+                        match event.code{
+                            KeyCode::Char('q') => {break;}
+                            _ => {}
+                    }
+
+
+
+                },
+                _ => {},
+            }
+        } else {
+            // Timeout expired and no `Event` is available
+        }
+
+
+
              draw(&sc , &world);
         };
-    
+    disable_raw_mode();
     Ok(())
 }
